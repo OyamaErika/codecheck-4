8f40deffe3ec8427f898ae199d313b5a4246786a
OyamaErika <oyama.erika@tis.co.jp>
2018-06-12 15:46:34 +0900

    新規作成

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -2,9 +2,22 @@ package codecheck;
 
 public class App {
 	public static void main(String[] args) {
-		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("argv[%s]: %s", i, args[i]);
-			System.out.println(output);
+		int num = Integer.parseInt(args[args.length-1]);
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0, l = args.length-1; i < l; i++) {
+			String[] str = args[i].split(":");
+			int key = Integer.parseInt(str[0]);
+			String value = str[1];
+
+			if (num % key == 0) {
+				sb.append(value);
+			}
+		}
+		if (sb.toString().isEmpty()) {
+			System.out.println(num);
+		} else {
+			System.out.println(sb);
 		}
 	}
 }
